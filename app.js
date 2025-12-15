4402bf22ba8f2afb1763e509d0894352e7d86f5d
Oleksii <gutsumori@gmail.com>
2025-12-16 01:05:14 +0200

    add home route

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.get("/", (req, res) => {
+  res.send("Jobs api");
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
